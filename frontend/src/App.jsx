d9d1a9555ed77972ed85682887d8b1e5f592ca04
Akash Krish <akashkrish1010@gmail.com>
2025-08-07 14:34:47 +0530

    udpated app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,26 +3,28 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './pages/Home';
-import ProtectedRoute from './components/ProtectedRoute.jsx'; 
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+import { Analytics } from '@vercel/analytics/react';
 
 
 const App = () => {
   return (
     <>
-    
-    <Routes>
-      
-      <Route
-        path="/"
-        element={
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+      <Analytics />
     </>
   );
 };
